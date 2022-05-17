6827646dde0beed23e7184688f14078f6d447f68
nohjonghun <nojong1005@gmail.com>
2022-05-17 23:22:59 +0900

    2022.05.17 스프링 핵심 원리 - 기본편 Commit - 어노테이션 생성 - 할인 정책 Qualifier, Primary 추가 - 동적 빈 활용을 통한 할인정책 다형성 테스트

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.FilterType;
 // 예제 코드의 유지를 위해 Configuration 어노테이션은 스캔에서 제외한다.
 @ComponentScan(
         // Default = 현재 Class의 패키지
-        basePackages = {"hello.core.order", "hello.core.member"},
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
