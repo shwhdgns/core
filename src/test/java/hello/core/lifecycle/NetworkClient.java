a384d3c81ee27df5ec446879e482740a7705e4c2
nohjonghun <nojong1005@gmail.com>
2022-05-23 21:39:02 +0900

    2022.05.23 스프링 핵심 원리 - 기본편 Commit - Bean Life Cycle 관리 - @Bean의 InitMethod와 DestroyMethod로 초기화와 종료 메서드 지정  -> Bean 어노테이션이 작성된 곳에만 적용이 가능하다는 단점이 존재한다.

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,6 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
 
@@ -27,16 +24,14 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close " + url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("afterPropertiesSet");
+    private void init() {
+        System.out.println("init Method");
         connect();
         call("초기화 연결 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("Destory");
+    private void close() {
+        System.out.println("Destory Method");
         disconnect();
     }
 }
