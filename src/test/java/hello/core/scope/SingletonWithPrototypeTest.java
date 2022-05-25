a664e74fcd8351dd92a0c6ed203cfd19e6e81d06
nohjonghun <nojong1005@gmail.com>
2022-05-26 00:01:32 +0900

    2022.05.25 스프링 핵심 원리 - 기본편 Commit - prototype Scope는 초기화(@PostConstruct)만 실행해주고 종료시점(@PreDestroy) 메서드는 실행하지 않는다.

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
