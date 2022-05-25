482a30cc056e486dc99f85773c1db0a52bfb811d
nohjonghun <nojong1005@gmail.com>
2022-05-25 22:32:06 +0900

    2022.05.25 스프링 핵심 원리 - 기본편 Commit - Bean Scope Singleton, Prototype 테스트 코드 작성

---
+++ src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,41 @@
+package hello.core.scope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PrototypeTest {
+
+    @Test
+    void prototypeBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        System.out.println(prototypeBean1);
+        System.out.println(prototypeBean2);
+        assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+
+        ac.close();
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("prototype Start");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            // prototype Scope에서는 동작 하지 않음.
+            // 실행을 원하는 경우 직접 호출해야한다.
+            System.out.println("Prototype Die.");
+        }
+    }
+}
